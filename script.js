38d880d2bd131a650b4b15e0c195cb249b5ffde9
Ved Prakash Pandey <75631832+vppandey56@users.noreply.github.com>
2023-11-07 12:47:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ btn.addEventListener("click", function () {
   if (flag == 0) {
     bulb.style.backgroundColor = "yellow";
     btn.innerHTML = "Off";
-    console.log("hi");
     flag = 1;
   } else {
     bulb.style.backgroundColor = "white";
     btn.innerHTML = "On";
-
-    console.log("hi");
     flag = 0;
   }
 });
